bd3a4c70fedae63fd66a1b53e8c537eef7d96e0b
EzequielBurg <ezekiel_sci@hotmail.com>
2021-11-07 14:49:43 -0300

    initial configs

---
+++ backend/src/routes/devs.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+// importar a controller
+
+export const devsRoutes = Router();
+
+// instanciar controller
+
+// devsRoutes.get();
+// devsRoutes.post();
+// devsRoutes.put();
+// devsRoutes.delete();
